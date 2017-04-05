9ce11c77dfe637c794e88f86ae8224c1c5120fa9
sumanthio <sumanthp544@gmail.com>
2017-04-05 23:06:17 +0530

    Angular toaster added. But that's redundant as ngMaterial has mdToast

--- app/index.js
+++ app/index.js
@@ -8,6 +8,8 @@ import 'angular-material/angular-material.css';
 
 import angularUIRouter from 'angular-ui-router';
 
+import toaster from 'angularjs-toaster';
+
 import TracksModule from './tracks/tracks.module';
 import GenresModule from './genres/genre.module';
 
@@ -23,14 +25,39 @@ let muzeApp = angular.module('muzeApp', [
   'ngAnimate',
   'ngMaterial',
   'ngResource',
+  toaster,
   TracksModule,
   GenresModule
 ]);
 
-muzeApp.config(['$urlRouterProvider', '$locationProvider', '$mdIconProvider', ($urlRouterProvider, $locationProvider, $mdIconProvider) => {
+muzeApp.config(['$urlRouterProvider', '$locationProvider', '$httpProvider', ($urlRouterProvider, $locationProvider, $httpProvider) => {
   $locationProvider.hashPrefix('');
   $urlRouterProvider.otherwise("/tracks/1");
-  $mdIconProvider.fontSet('fa', 'font-awesome');
+
+  $httpProvider.interceptors.push(function ($q, $injector,) {
+    return {
+      'request': function (config) {
+        return config;
+      },
+
+      'requestError': function (rejection) {
+                return $q.reject(rejection);
+      },
+
+      'response': function (response) {
+        return response;
+      },
+
+      'responseError': function (rejection) {
+        if(rejection.status > 400) {
+                    toaster.pop('error', "Error", "Something went wrong");
+                }
+        return $q.reject(rejection);
+      }
+    };
+  });
+
+
 }]);
 
 muzeApp.controller('SideBarCtrl', SideBarCtrl);
